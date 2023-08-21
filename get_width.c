7af6148aadfab50684cdd27a22213e61e214b516
Cosmas <cosmasnnabuike@gmail.com>
2023-08-21 08:49:11 -0700

    width

---
+++ get_width.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * get_width - Calculates width to be printed
+ * @format: Formatted string that print the arguments.
+ * @x: arguments to be printed.
+ * @list: list arguments.
+ *
+ * Return: width.
+ */
+int get_width(const char *format, int *x, va_list list)
+{
+	int curr_x;
+	int width = 0;
+
+	for (curr_x = *x + 1; format[curr_x] != '\0'; curr_x++)
+	{
+		if (is_digit(format[curr_x]))
+		{
+			width *= 10;
+			width += format[curr_x] - '0';
+		}
+		else if (format[curr_x] == '*')
+		{
+			curr_x++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*x = curr_x - 1;
+
+	return (width);
+}
