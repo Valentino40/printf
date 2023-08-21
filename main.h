700972f3481b57508b45d242233fd09c1cfde349
Cosmas <cosmasnnabuike@gmail.com>
2023-08-21 04:24:36 -0700

    mainh

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+
+#endif
