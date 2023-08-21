3f8586c31a433429528e10c7233e65687603cc30
Cosmas <cosmasnnabuike@gmail.com>
2023-08-21 04:56:05 -0700

    mainh

--- main.h
+++ main.h
@@ -7,6 +7,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 int _printf(const char *format, ...);
 
 #endif
